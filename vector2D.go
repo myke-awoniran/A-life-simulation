01f96db6b1b457f57b5b80bf78535800beddb0fb
Awoniran <awoniranopeyemi@gmail.com>
2023-01-24 23:55:59 +0100

    first commit

---
+++ vector2D.go
@@ -0,0 +1,40 @@
+package main
+
+import "math"
+
+type Vector struct {
+	x float64
+	y float64
+}
+
+func (v1 Vector) Add(v2 Vector) Vector {
+	return Vector{x: v1.x + v2.x, y: v2.y + v1.y}
+}
+
+func (v1 Vector) Subtract(v2 Vector) Vector {
+	return Vector{x: v1.x - v2.x, y: v1.y - v2.y}
+}
+
+func (v1 Vector) Multiply(v2 Vector) Vector {
+	return Vector{x: v1.x * v2.x, y: v1.y * v2.y}
+}
+
+func (v1 Vector) AddV(d float64) Vector {
+	return Vector{x: v1.x + d, y: v1.y + d}
+}
+
+func (v1 Vector) MultiplyV(d float64) Vector {
+	return Vector{x: v1.x * d, y: v1.y * d}
+}
+
+func (v1 Vector) DivisionV(d float64) Vector {
+	return Vector{x: v1.x / d, y: v1.y / d}
+}
+
+func (v1 Vector) limit(lower, upper float64) Vector {
+	return Vector{x: math.Min(math.Max(v1.x, lower), upper), y: math.Min(math.Max(v1.y, lower), upper)}
+}
+
+func (v1 Vector) Distance(v2 Vector)Vector{
+	return math.Sqrt(math.Pow(v1.x-v2.x, y:2) + math.Pow(v1.y-v2.y, y:2))
+}
