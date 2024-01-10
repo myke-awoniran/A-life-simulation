fd32f38c6814fb97d8c7011c0cc4834b270e404b
myke <awoniranopeyemi@gmail.com>
2024-01-10 02:56:43 +0100

    complete setup

--- multithreading/boid.go
+++ multithreading/boid.go
@@ -1,7 +1,44 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 type Boid struct {
 	position Vector2D
 	velocity Vector2D
 	id       int
+}
+
+func (boid *Boid) MoveOne() {
+	boid.position = boid.position.Add(boid.velocity)
+	next := boid.position.Add(boid.velocity)
+
+	if next.x > screenWidth || next.x < 0 {
+		boid.velocity = Vector2D{-boid.velocity.x, boid.velocity.y}
+	}
+
+	if next.y > screenHeight || next.y < 0 {
+		boid.velocity = Vector2D{boid.velocity.x, -boid.velocity.y}
+	}
+}
+
+func (boid *Boid) Start() {
+	for {
+		boid.MoveOne()
+		time.Sleep(5 * time.Millisecond)
+	}
+}
+
+func createBoid(boidId int) {
+	boid := Boid{
+		position: Vector2D{rand.Float64() * screenWidth, rand.Float64() * screenHeight},
+		velocity: Vector2D{(rand.Float64() * 2) - 1.0, (rand.Float64() * 2) - 1.0},
+		id:       boidId,
+	}
+
+	boids[boidId] = &boid
+
+	go boid.Start()
 }
---
+++ multithreading/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+	"log"
+)
+
+const (
+	screenWidth, screenHeight = 640, 360
+	boidCount                 = 500
+)
+
+var (
+	green = color.RGBA{10, 255, 50, 255}
+	boids = [boidCount]*Boid{}
+)
+
+type Game struct {
+}
+
+func (game *Game) Update() error {
+	return nil
+}
+
+func (game *Game) Draw(screen *ebiten.Image) {
+	for _, boid := range boids {
+		screen.Set(int(boid.position.x+1), int(boid.position.y+1), green)
+		screen.Set(int(boid.position.x-1), int(boid.position.y), green)
+		screen.Set(int(boid.position.x), int(boid.position.y-1), green)
+		screen.Set(int(boid.position.x), int(boid.position.y+1), green)
+
+	}
+}
+
+func (game *Game) Layout(_, _ int) (width, height int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+
+	for i := 0; i < boidCount; i++ {
+		createBoid(i)
+	}
+	ebiten.SetWindowSize(screenWidth*2, screenHeight)
+	ebiten.SetWindowTitle("Boids in a box")
+
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
